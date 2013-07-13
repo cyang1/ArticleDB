0d725b56937f8e33eaf9f686ad723b3cf56a7a60
Cary Yang <caryy@cmu.edu>
2013-07-12 21:14:22 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArticleDB::Application.config.secret_token = '2687e04773d30f37495e750b08b3c5e94ee046b1bea24119be78d27fa56342a93b1847c0ec162e212e9585421a2623e48f1af36218351706d558f48b9fd8124d'
